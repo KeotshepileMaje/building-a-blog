4cd084fa66ad2042f3264f47691db63f9f268152
keotshepileMaje <keotshepilemaje@gmail.com>
2023-09-27 14:37:52 +0200

    Set next.config for better environment handling

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = nextConfig
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   env: {
+//     mongodb_username: 'keotshepilemaje',
+//     mongodb_password: 'gZGT5QdpzsMChhUH',
+//     mongodb_clustername: 'cluster0',
+//     mongodb_database: 'my-blog',
+//   }
+// }
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: 'keotshepilemaje',
+        mongodb_password: 'gZGT5QdpzsMChhUH',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-blog-dev',
+      }
+    }
+  }
+  
+  return {
+    env: {
+      mongodb_username: 'keotshepilemaje',
+      mongodb_password: 'gZGT5QdpzsMChhUH',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'my-blog',
+    }
+  }
+}
