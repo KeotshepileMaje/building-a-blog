bd243f80b4df7aa8d80c5bf399020e46e0535a56
keotshepileMaje <keotshepilemaje@gmail.com>
2023-09-27 16:03:07 +0200

    Fixed imports

--- components/ui/Notification.js
+++ components/ui/Notification.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import classes from './notification.module.css';
+import classes from './Notification.module.css';
 
 export default function Notification(props) {
 
