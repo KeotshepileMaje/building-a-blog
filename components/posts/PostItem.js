bcb68f672c25d091af04fdb13a38ace231ddc011
keotshepileMaje <keotshepilemaje@gmail.com>
2023-10-07 09:40:59 +0200

    Fixed image links

--- components/posts/PostItem.js
+++ components/posts/PostItem.js
@@ -10,7 +10,7 @@ export default function PostItem(props) {
         month: 'long',
         year: 'numeric'
     })
-    const imagePath = `/images/posts/${image}`
+    const imagePath = `/images/posts/${slug}/${image}`
     const linkPath =  `/posts/${slug}`
 
     return (
