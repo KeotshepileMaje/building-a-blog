0cfe80d0a08f48b72a9915dcebffbe915eaf33b2
keotshepileMaje <keotshepilemaje@gmail.com>
2023-10-07 10:02:52 +0200

    Added comment section

---
+++ components/comment-section/comment-list.js
@@ -0,0 +1,19 @@
+import classes from './comment-list.module.css';
+
+function CommentList(props) {
+  const {items} = props
+
+  if (!items) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <ul className={classes.comments}>
+      {items.map(
+        (item)=> <li key={item._id}>{item.text}</li>
+      )}
+    </ul>
+  );
+}
+
+export default CommentList;
---
+++ pages/comments.js
@@ -0,0 +1,9 @@
+import Comments from "@/components/comment-section/comments";
+
+export default function CommentsPage() {
+    return (
+        <h1>
+            <Comments />
+        </h1>
+    )
+}
