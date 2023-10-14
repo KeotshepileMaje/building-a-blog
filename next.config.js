2930faedae09c4576232fcf752c228ce28775da0
keotshepileMaje <keotshepilemaje@gmail.com>
2023-10-14 14:38:04 +0200

    Remover commented code

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   env: {
-//     mongodb_username: 'keotshepilemaje',
-//     mongodb_password: 'gZGT5QdpzsMChhUH',
-//     mongodb_clustername: 'cluster0',
-//     mongodb_database: 'my-blog',
-//   }
-// }
-
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
